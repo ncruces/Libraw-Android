e01d6ba111718b53480be3c953154a9337059106
Nuno Cruces <ncruces@gmail.com>
2019-03-20 00:52:05 +0000

    Use CMake.

--- app/src/main/ndk/Libraw_Open/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-# Build both ARMv5TE and ARMv7-A machine code.
-APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
-
-APP_STL := c++_shared
-APP_CPPFLAGS := -frtti -fexceptions -latomic
-APP_OPTIM := release
